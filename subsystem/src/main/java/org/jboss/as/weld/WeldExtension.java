7270f043be386ca65076451a6fa8efd4ff3c31b8
Jozef Hartinger <jharting@redhat.com>
2012-10-15 16:46:14 +0200

    Copy the util method in order to stay compatible with AS 7.1.1.Final

--- subsystem/src/main/java/org/jboss/as/weld/WeldExtension.java
+++ subsystem/src/main/java/org/jboss/as/weld/WeldExtension.java
@@ -22,6 +22,16 @@
 
 package org.jboss.as.weld;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DESCRIBE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+import static org.jboss.as.controller.parsing.ParseUtils.requireNoAttributes;
+import static org.jboss.as.controller.parsing.ParseUtils.requireNoContent;
+
+import java.util.List;
+
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+
 import org.jboss.as.controller.Extension;
 import org.jboss.as.controller.ExtensionContext;
 import org.jboss.as.controller.PathAddress;
@@ -32,25 +42,16 @@ import org.jboss.as.controller.SimpleResourceDefinition;
 import org.jboss.as.controller.SubsystemRegistration;
 import org.jboss.as.controller.descriptions.StandardResourceDescriptionResolver;
 import org.jboss.as.controller.operations.common.GenericSubsystemDescribeHandler;
-import org.jboss.as.controller.operations.common.Util;
 import org.jboss.as.controller.parsing.ExtensionParsingContext;
 import org.jboss.as.controller.persistence.SubsystemMarshallingContext;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.jboss.as.controller.registry.OperationEntry;
+import org.jboss.as.weld.util.Operations;
 import org.jboss.dmr.ModelNode;
 import org.jboss.staxmapper.XMLElementReader;
 import org.jboss.staxmapper.XMLElementWriter;
 import org.jboss.staxmapper.XMLExtendedStreamReader;
 import org.jboss.staxmapper.XMLExtendedStreamWriter;
-
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.XMLStreamException;
-import java.util.List;
-
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DESCRIBE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
-import static org.jboss.as.controller.parsing.ParseUtils.requireNoAttributes;
-import static org.jboss.as.controller.parsing.ParseUtils.requireNoContent;
 
 /**
  * Domain extension used to initialize the weld subsystem.
@@ -114,7 +115,7 @@ public class WeldExtension implements Extension {
             // Require no attributes or content
             requireNoAttributes(reader);
             requireNoContent(reader);
-            list.add(Util.createAddOperation(PathAddress.pathAddress(PATH_SUBSYSTEM)));
+            list.add(Operations.createAddOperation(PathAddress.pathAddress(PATH_SUBSYSTEM)));
         }
 
         /** {@inheritDoc} */
